5b1aa092b0d4ff55b41c5bbc265bc0bee29c22c3
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-09-11 17:37:55 +0530

    Added new game button.

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class App(object):
 
         self.menu = Menu(self.display)
         self.menu.on_resume_click = self.on_resume_click
+        self.menu.on_new_click = self.on_new_click
 
         self.current_state = self.game
 
@@ -78,6 +79,9 @@ class App(object):
 
     def on_resume_click(self, event):
         self.current_state = self.game
+
+    def on_new_click(self, event):
+        print('New game')
 
 
 class Game(object):
@@ -232,6 +236,8 @@ class Menu(object):
     def __init__(self, display):
         self.display = display
 
+        # The title is a bit bigger in the menu, than the game.
+        # FIXME: Remove repetition with the title display code in `Game`.
         title_font = pygame.font.Font('Signika-Regular.ttf', 48)
         self.title_surface = title_font.render('Lights poof!', True,
                 (213, 85, 148))
@@ -239,18 +245,19 @@ class Menu(object):
         self.title_rect.centerx = self.display.get_width() / 2
         self.title_rect.top = 18
 
+        # Maintain a list of buttons in this menu.
         self.buttons = []
 
-        resume_btn = Button('Resume', centerx=self.display.get_width() / 2)
-        resume_btn.on_click = (lambda event:
-                self.on_resume_click and self.on_resume_click(event))
-        self.buttons.append(resume_btn)
+        # Create the buttons.
+        self.new_button('New Game', 'on_new_click')
+        self.new_button('Resume', 'on_resume_click')
 
+        # Incrementally set the `y` coordinate of the buttons.
         y = self.title_rect.top + self.title_rect.height + 24
         button_gap = 12
         for button in self.buttons:
             button.update_rect(y=y)
-            y += button_gap
+            y += button.rect.height + button_gap
 
     def draw(self):
         # Draw the title.
@@ -262,6 +269,13 @@ class Menu(object):
     def handle(self, event):
         for button in self.buttons:
             button.handle(event)
+
+    def new_button(self, label, handler_name=None):
+        button = Button(label, centerx=self.display.get_width() / 2)
+        self.buttons.append(button)
+
+        if handler_name:
+            button.on_click = (lambda event: getattr(self, handler_name)(event))
 
 
 class Button(object):
